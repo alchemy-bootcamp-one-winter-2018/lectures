88732fd21889e3795949b7bb56a4f8c31aefa92b
Sarah Joy Calpo <sajoy.pc@gmail.com>
2018-02-01 12:08:21 -0800

    update class 4

--- class-04-functions-css_layout/demo/app.js
+++ class-04-functions-css_layout/demo/app.js
@@ -3,15 +3,49 @@
 /* what's a function and why use it?? */
 
 console.log('lol');
-alert('hahahahahaha');
+// alert('hahahahahaha');
 
-/* anatomy of a function */
+// anatomy of a function
 
-function shout (word) {
+// using a function
+// alert('this is an argument');
+whisper();
+whisperThis('potato');
+whisperThis('carrot');
+whisperThis();
+
+// creating a function
+function whisper() {
+    console.log('whisper somethinnnggggg');
+}
+
+function whisperThis(whateverWeWant) {
+    // whateverWeWant = 'carrot';
+    console.log(whateverWeWant);
+}
+
+function add(number, otherNumber) {
+    // would add number and otherNumber
+    for (let i = 0; i < number; i++) {
+        alert(number + otherNumber);
+    }
+
+    return number + otherNumber;
+}
+
+const sum = add(2,8);
+console.log('the sum is: ' + sum);
+
+// add(17,90);
+
+
+// const hi = prompt('hey');
+
+function shout(word) {
     let newWord = 'ahhhh' + word;
 
     if (word.length < 5) {
-        newWord = newWord.toUpperCase(); 
+        newWord = newWord.toUpperCase();
     } else {
         newWord = newWord.toLowerCase();
     }
@@ -20,15 +54,30 @@ function shout (word) {
 }
 
 
-/* make code happen when we want it to */
+const hi = shout('hello');
+console.log('hi = ' + hi);
 
+const hey = shout('hey');
+console.log('hey = ' + hey);
 
+// make code happen when we want it to
+// make things DRY!!!
 
-/* make things DRY!!! */
 
 
 /* function declaration vs function expression */
+// function declarations! can be used before its declared in the code
 
 // function expression can only be called after being saved in a variable
 
-// function declarations! can be used before its declared in the code
+// will through an error because eat hasn't been created yet 
+// eat();
+
+const eat = function (yo) {
+    console.log('let us eat cake!');
+    console.log(yo);
+};
+
+eat();
+
+whisper();
