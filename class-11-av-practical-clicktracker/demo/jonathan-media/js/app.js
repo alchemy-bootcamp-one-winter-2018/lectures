f4788b1f64298e27702611019cdbbe1f67a98968
Sarah Joy Calpo <sajoy.pc@gmail.com>
2018-02-12 11:55:26 -0800

    update demo

--- class-11-av-practical-clicktracker/demo/jonathan-media/js/app.js
+++ class-11-av-practical-clicktracker/demo/jonathan-media/js/app.js
@@ -1,1 +1,12 @@
 // TODO play or pause the video using JS
+
+const h1 = document.querySelector('h1');
+h1.addEventListener('click', function () {
+    const video = document.querySelector('video');
+    
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+});
--- class-11-av-practical-clicktracker/demo/jonathan-media/js/collect.js
+++ class-11-av-practical-clicktracker/demo/jonathan-media/js/collect.js
@@ -1,3 +1,15 @@
+/*
+    create a game where users can "Catch" cuttlefish by clicking on them.
+    - display (3) random fish for a user to try to catch
+    - user can click on fish to catch it
+
+    - cuttlefish will move around the screen
+    - cuttlefish will be worth a certain number of points
+    - when a round is done, tally points of all fish caught
+
+*/
+
+
 const game = {
     cuttlefish: [],
     start: function () {
@@ -16,20 +28,95 @@ const game = {
 
         // show 3 random cuttlefish
         this.showFish();
+
+        const board = document.getElementById('game-board');
+        board.addEventListener('click', function () {
+            console.log('game was clicked', event.target);
+            // what to do when image is clicked
+            // track # of clicks per image
+            // increase the clicked fish's .timesCaught property
+            
+            const url = event.target.src;
+            for(let i = 0; i < game.cuttlefish.length; i ++) {
+                const fish = game.cuttlefish[i];
+                
+                console.log('index of url', url.indexOf(fish.imageUrl));
+                const endOfUrl = url.slice( url.indexOf(fish.imageUrl), url.length );
+                // debugger;
+
+                if (endOfUrl === fish.imageUrl) {
+                    fish.timesCaught++;
+                    console.table(fish);
+                }
+            }
+
+            // reselect and append new images
+            game.clearBoard();
+            game.showFish(); // what is this
+        });
+    },
+    getRandomFish: function () {
+        const selectedFishes = [];
+        for (let i = 0; i < 3; i++) {
+            const randomNumber = Math.floor(Math.random() * this.cuttlefish.length);
+            const fish = this.cuttlefish[randomNumber];
+            selectedFishes.push(fish);
+
+            // const fishEle = fish.render();
+            // section.appendChild(fishEle);
+            // section.appendChild(fish.render());
+        }
+        console.table(selectedFishes);
+
+        return selectedFishes;
+    },
+    getRandomSquares: function () {
+        const section = document.getElementById('game-board');
+        const allSquares = document.querySelectorAll('div.four');
+        const selectedSquares = [];
+        while(selectedSquares.length < 3) {
+            // get random square
+            const randomNumber = Math.floor(Math.random() * allSquares.length);
+            const square = allSquares[randomNumber];
+            if (selectedSquares.includes(square)) continue;
+            selectedSquares.push(square);
+        }
+
+        return selectedSquares;
     },
     showFish: function () {
         // get three fish
+        const fishes = this.getRandomFish();
+
         // get three squares
+        const squares = this.getRandomSquares();
 
         // add fish to squares
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].appendChild(fishes[i].render());
+        }
+    },
+    clearBoard: function () {
+        const allSquares = document.querySelectorAll('div.four');
+        for (let i = 0; i < allSquares.length; i ++) {
+            allSquares[i].textContent = '';
+        }
     }
 };
 
+
+
 function Cuttlefish (name, imageUrl) {
     this.name = name;
-    this.id = name.replace(/\s/g, '-');
     this.imageUrl = imageUrl;
-    this.count = 0;
+    this.timesCaught = 0;
 }
+
+Cuttlefish.prototype.render = function () {
+    const ele = document.createElement('img');
+    ele.src =  `media/images/${this.imageUrl}`;
+    ele.setAttribute('alt', this.name);
+    return ele;
+};
 
 game.start();
